43da4f6328c3f84e34007a409d0a5aedcf0ad75d
Veto Ramirez <vetorobert3@gmail.com>
2020-02-29 10:48:22 -0800

    Creating listing form.

--- ReactRV/src/components/ListingForm.js
+++ ReactRV/src/components/ListingForm.js
@@ -3,7 +3,23 @@ import React from 'react';
 const ListingForm = () => {
   return (
     <div>
-      <h1>Listing Form</h1>
+      <h1>Book Your Adventure Here!</h1>
+      <form>
+        <label>
+          Where To?
+            <input type='text' placeholder='Where to?' />
+        </label>
+        <div>
+          <label>
+            Check-In:
+              <input type='text' placeholder='MM/DD/YY' />
+          </label>
+          <label>
+            Check-Out:
+              <input type='text' placeholder='MM/DD/YY' />
+          </label>
+        </div>
+      </form>
     </div>
   )
 }
